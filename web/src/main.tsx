f6a17e18b163f4758596533ea4277ee2f02fad33
muggle <promuggle@gmail.com>
2026-07-12 04:04:01 +0800

    fix(web): include goal and status component styles

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./App.css";
 import App from "./App";
 import { ErrorBoundary } from "./ErrorBoundary";
 import { useMobileViewport } from "./use-mobile-viewport";
